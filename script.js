7048ae358adedb2bb4b84ab1e98e10a4aa8b8a95
Filip Kaczmarek <61003790+FilipKaczmarek@users.noreply.github.com>
2020-08-03 18:32:56 +0200

    Update
    added some things in css/js + RWD

--- script.js
+++ script.js
@@ -4,23 +4,37 @@ const texts = ['Filip.', 'a Front-End Developer.', 'a Podcaster.', 'a Recruiter.
 let count = 0; // each induvidual text
 let index = 0; // track induvidual letter
 let currentText = ""; // text currently selected 
-let letter = ""; 
+let letter = "";
 
-(function type(){
-    if(count === texts.length){
+(function type() {
+    if (count === texts.length) {
         count = 0;
     }
     currentText = texts[count];
     letter = currentText.slice(0, ++index);
 
     document.querySelector(".typing").textContent = letter;
-    if(letter.length === currentText.length){
+    if (letter.length === currentText.length) {
         count++;
         index = 0;
     }
 
     setTimeout(type, 400);
 }());
+
+// JQuery Smooth Scroll
+$('.list-item a').on('click', function (e) {
+    if (this.hash !== '') {
+        e.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800
+        );
+    }
+});
 
 
 // PROGRESS BARS
@@ -49,3 +63,20 @@ setTimeout(() => {
     progressSass.style.width = progressSass.getAttribute('data-done') + "%";
     progressSass.style.opacity = 1;
 }, 500);
+
+
+
+
+const aboutMeSection = document.querySelector("#about-me").getBoundingClientRect().top;
+const navBar = document.querySelector(".navbar-view");
+
+window.addEventListener('scroll', () => {
+
+    let windowHeight = window.pageYOffset;
+
+    if (windowHeight > aboutMeSection) {
+        navBar.classList.add("navbar-scroll");
+    } else {
+        navBar.classList.remove("navbar-scroll");
+    };
+})
