e893c7cdde63c64b58589120b7a2e9904d0eac21
Filip Kaczmarek <61003790+FilipKaczmarek@users.noreply.github.com>
2020-08-21 08:45:25 +0200

    Update 21.08.2020

---
+++ pl/script-plVersion.js
@@ -0,0 +1,23 @@
+// --- TYPING - MAIN PAGE ---
+
+const texts = ['Filip.', 'Front-End Developer.', 'podcasterem.', 'rekruterem.'];
+let count = 0; // each induvidual text
+let index = 0; // track induvidual letter
+let currentText = ""; // text currently selected 
+let letter = "";
+
+(function type() {
+    if (count === texts.length) {
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector(".typing").textContent = letter;
+    if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+    }
+
+    setTimeout(type, 300);
+}());
--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// --- TYPING - MAIN PAGE ---
-
-const texts = ['Filip.', 'a Front-End Developer.', 'a Podcaster.', 'a Recruiter.'];
-let count = 0; // each induvidual text
-let index = 0; // track induvidual letter
-let currentText = ""; // text currently selected 
-let letter = "";
-
-(function type() {
-    if (count === texts.length) {
-        count = 0;
-    }
-    currentText = texts[count];
-    letter = currentText.slice(0, ++index);
-
-    document.querySelector(".typing").textContent = letter;
-    if (letter.length === currentText.length) {
-        count++;
-        index = 0;
-    }
-
-    setTimeout(type, 400);
-}());
-
 // JQuery Smooth Scroll
 $('.list-item a').on('click', function (e) {
     if (this.hash !== '') {
